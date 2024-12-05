4565d0d77578afd89e4203ce8aaac7b77d11f927
lindsayh17 <lhall11@uvm.edu>
2024-12-05 14:00:30 -0500

    documentation

--- login.py
+++ login.py
@@ -52,33 +52,36 @@ def login():
         lock_status = check_locked(get_id(username))
         # check to see if account is already locked
         if lock_status == True:
-            return render_template('locked.html',
-                                   title="Secure Login",
-                                   heading="Secure Login")
+            return redirect(url_for('locked'))
+        # display error page
         elif lock_status != False:
             flash(lock_status, 'alert-danger')
-            return render_template('error.html',
-                                   title="Secure Login",
-                                   heading="Secure Login")
+            return redirect(url_for('error'))
         try:
             # pass in 80 because 1 byte = 2 hex values
             if authenticate(pw_hash, password, 80):
+                # set attempts back to 0
                 session['attempts'] = 0
                 return redirect(url_for('options',
                                         id_=get_id(username)))
             else:
+                # increment attempts
                 session['attempts'] += 1
 
                 # lock account after 3 attempts
                 if session['attempts'] > 2:
+                    # lock account
                     lock_success = lock(username)
+                    # check to see if the account was successfully locked
                     if lock_success == True:
                         flash("Your account has been locked.", 'alert-danger')
                         session['attempts'] = 0
                         return redirect(url_for('locked'))
+                    # throw error if not successfully locked
                     elif lock_success != False:
                         flash(lock_success, 'alert-danger')
                         return redirect(url_for('error'))
+                # if attempts has not been exceeded
                 else:
                     flash("Invalid username or password!",
                           'alert-danger')
@@ -96,12 +99,15 @@ def register():
         username = request.form.get('username')
         password = request.form.get('password')
 
+        # tell user if chosen username already exists
         if check_exist(username):
             flash("Username already exists!", 'alert-info')
+        # ensure password is strong enough
         elif not password_strength(password):
             flash("Password is too weak. Your password must have 8-25 characters and at least one of each of the "
                   "following: lowercase, uppercase, number, special character (!@#$%^&*)", 'alert-info')
         else:
+            # hash the password then add user
             hashed_password = hash_pw(password)
             add_success = add_user(username, hashed_password, 's')
             if add_success == True:
@@ -116,6 +122,7 @@ def register():
 
 @app.route("/options/<int:id_>", methods=['GET', ])
 def options(id_):
+    """ User menu """
     flash("Welcome! You have logged in!", 'alert-success')
 
     return render_template('options.html',
@@ -125,7 +132,9 @@ def options(id_):
 
 @app.route("/option_page/<int:option_id>", methods=['GET', ])
 def option_page(option_id):
+    """ Show page for chosen option """
     flash("Welcome! You have logged in!", 'alert-success')
+    # get correct options based on id
     option_menu = [[4, 'Time Report'], [1, 'Pay'], [2, 'Rosters'], [3, 'Grades'], [0, 'Time Off'], [5, 'Evals']]
     option = option_menu[option_id]
     return render_template('option_page.html',
@@ -135,12 +144,14 @@ def option_page(option_id):
 
 @app.route("/locked", methods=['GET', 'POST'])
 def locked():
+    """ Message saying the account was locked """
     return render_template('locked.html',
                            title="Account Locked",
                            heading="Account Locked")
 
 @app.route("/error", methods=['GET', 'POST'])
 def error():
+    """ Error message """
     return render_template('error.html',
                            title="Error",
                            heading="Error")
