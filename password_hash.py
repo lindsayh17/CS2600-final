6f0b30d10ab073ce198a9f25720802c11d691b61
lindsayh17 <lhall11@uvm.edu>
2024-12-05 22:01:20 -0500

    documentation

--- password_hash.py
+++ password_hash.py
@@ -1,41 +1,17 @@
 """"
-Password salting and hashing example
+Password salting and hashing
 
-Sources: https://www.geeksforgeeks.org/how-to-convert-bytes-to-string-in-python/
 """
 
 import hashlib
-import os  # << hint
+import os
 
 
-def hash_pw(plain_text, salt='') -> str:
+def hash_pw(plain_text) -> str:
     """
-    Generate hash of plain text. Here we allow for passing in a salt
-    explicitly. This is so you can tinker and see the results.
-
-    Python's Hashlib provides all we need here. Documentation is at
-    https://docs.python.org/3/library/hashlib.html.
-
-    Here we use SHA-1. (Weak!) For stronger encryption, see: bcrypt,
-    scrypt, or Argon2. Nevertheless, this code should suffice for an
-    introduction to some important concepts and practices.
-
-    A few things to note.
-
-    If we supply a fixed salt (or don't use a salt at all), then the
-    output of the hash function becomes predictable -- for a given
-    algorithm, the same password will always produce the same result.
-
-    If we allow our algorithm to generate a salt from a pseudorandom
-    input (e.g., using os.urandom(60)) then the same password will
-    produce different results. All we know is the length of the combined
-    salt and password.
-
-    If we wish to be able to authenticate, then we must store the salt
-    with the hash. We facilitate this by prepending the salt to the hash.
+    Generates a hash of plain text using SHA-1.
 
     :param plain_text: str (user-supplied password)
-    :param salt: str
     :return: str (ASCII-encoded salt + hash)
     """
 
